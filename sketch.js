79420e5349dddc3ff21ef9581b90aa13570df0c6
Leadedjargon1979 <108930236+Leadedjargon1979@users.noreply.github.com>
2022-07-08 19:58:56 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("My name is Abbas");
+console.log("I come from Hyderabad,India.");
+console.log("My hobbies are to play sports and video games.");
+console.log("My favourite cricketer is Virat Kohli");
